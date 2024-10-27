448a7917fe370588d715b760705c625b7adbdd06
Christopher X Gerber <christopher.x.gerber@gmail.com>
2024-10-27 12:01:17 -0700

    fix: adjust DOM keywords to match recent Google Calendar update

--- contentScript.js
+++ contentScript.js
@@ -22,6 +22,9 @@ var observeDOM = (function() {
 })()
 
 const mutateTime = async function(containerElementJSName, mutationInMinutes) {
+  // Grab whitespace to click out of input later
+  let modalWhiteSpace = document.querySelectorAll("[jsname='uxAMZ']")[0] // uxAMZ is the jsname value of the div containing the white space of the part of the modal that doesn't contain the title input or the buttons at the bottom
+
   // Mutate iCal data attribute
   const containerElement = document.querySelectorAll(`[jsname="${containerElementJSName}"]`)[0]
   const originalIcalValue = containerElement.getAttribute('data-ical')
@@ -34,14 +37,13 @@ const mutateTime = async function(containerElementJSName, mutationInMinutes) {
   const newValue = "whatever"
   inputElement.setAttribute('data-initial-value', newValue)
 
-  await new Promise(r => setTimeout(r, wait))
-
   // Click in and out of the input to trigger Google UI scripts (simulate real user input)
   inputElement.click()
-  const modalWhiteSpace = document.querySelectorAll("[jsname='uxAMZ']")[0]
-  modalWhiteSpace.dispatchEvent(new MouseEvent('mousedown'))
 
   await new Promise(r => setTimeout(r, wait));
+  console.log("Clicking out of input")
+  modalWhiteSpace.dispatchEvent(new MouseEvent('mousedown'))
+  modalWhiteSpace.dispatchEvent(new MouseEvent('mouseup'))
 }
 
 const callback = function() {
@@ -51,14 +53,16 @@ const callback = function() {
     if (modalVisible) {
 
       // 1. Expand datetime section of modal
-      const timeslotButton = document.querySelectorAll('[jsname="GIdnzc"] button')[0]
+      const timeslotButton = document.querySelectorAll('[jsname="TAmOZ"] button')[0] // TAmOZ is the jsname value of the dev containing the button that expands the datetime section of the modal
       timeslotButton.click()
 
       chrome.storage.sync.get("startTimeOffsetInMinutes", async data => {
         const offset = parseInt(data.startTimeOffsetInMinutes)
 
         // 2. Adjust start-time
-        await mutateTime("B4GDSd", offset)
+        await new Promise(r => setTimeout(r, wait))
+        console.log("Adjusting start-time")
+        await mutateTime("B4GDSd", offset) // B4GDSd is the jsname value of the div containing the tree that contains the start-time input
 
         chrome.storage.sync.get("persistDuration", async data => {
           const persist = !!data.persistDuration
@@ -66,11 +70,15 @@ const callback = function() {
 
             // 3. Adjust the subsequently changed end-time back to what it was
             //    (Google tries to preserve the duration of the event, so when the start time changes, so does the end time)
-            await mutateTime("XCHdmd", -offset)
+            await new Promise(r => setTimeout(r, wait))
+            console.log("Resetting end-time")
+            await mutateTime("XCHdmd", -offset) // XCHdmd is the jsname value of the div containing the tree that contains the end-time input
           }
 
           // 4. Focus back to the "title" input
-          const titleElement = document.querySelectorAll(`[jsname="Y9wHSb"] input`)[0]
+          await new Promise(r => setTimeout(r, wait))
+          console.log("Giving focus back to Title input")
+          const titleElement = document.querySelectorAll(`[jsname="GYcwYe"] > [jsname="vhZMvf"] > input`)[0] // GYcwYe and vhZMvf are jsname values in the div tree that contains the title input
           titleElement.focus()
         })
       })
@@ -80,7 +88,7 @@ const callback = function() {
 
 
 let modalVisible = false
-const wait = 50
-const modalContainer = document.getElementById("yDmH0d")
+const wait = 100
+const modalContainer = document.getElementById("yDmH0d") // yDmH0d is the jsname value of the div containing the DOM tree that is the modal (it is visible but empty when the model is not visible)
 observeDOM(modalContainer, callback)
 
